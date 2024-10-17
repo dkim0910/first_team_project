e1f555c4f00d2bdc73df413e189ca8212119beb0
dkim0910 <dkimdaniel0911@gmail.com>
2024-10-17 15:36:24 +0900

    Delete Project2 directory

--- Project2/src/main/java/com/kh/mypage/refund/Action/Action.java
+++
@@ -1,8 +0,0 @@
-package com.kh.mypage.refund.Action;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Action {
-    ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
